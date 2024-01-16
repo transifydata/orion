67d47cdc8a26425d8d6e3db319cb7ab7c2a52c26
Henry Nguyen <henry2833@gmail.com>
2024-01-15 20:32:10 -0800

    measure execution time for scheduled + live feeds

--- src/get-vehicle-locations.ts
+++ src/get-vehicle-locations.ts
@@ -1,19 +1,31 @@
-import {getScheduledVehicleLocations} from "./get-scheduled-vehicle-locations";
-import {getLiveVehicleLocations} from "./get-live-vehicle-locations";
-import {VehiclePositionOutput} from "./providers/gtfs-realtime";
-
+import { getScheduledVehicleLocations } from "./get-scheduled-vehicle-locations";
+import { getLiveVehicleLocations } from "./get-live-vehicle-locations";
+import { VehiclePositionOutput } from "./providers/gtfs-realtime";
 
 export interface LinkedPosition {
-    live?: VehiclePositionOutput;
-    scheduled: VehiclePositionOutput;
+  live?: VehiclePositionOutput;
+  scheduled: VehiclePositionOutput;
 }
 
 export interface LinkedPositionsOutput {
     [tripId: string]: LinkedPosition;
 }
+
+async function measureExecutionTime<T>(func: () => Promise<T>): Promise<{ time: number, result: T }> {
+  const startTime = performance.now();
+
+  // Execute the provided function
+  const result = await func();
+
+  const endTime = performance.now();
+  return {result, time: endTime - startTime};
+}
+
 export default async function getVehicleLocations(agency: string, time: number): Promise<LinkedPositionsOutput> {
-    const scheduledPositions = await getScheduledVehicleLocations(agency, time);
-    const livePositions = await getLiveVehicleLocations(agency, time);
+    const {result: scheduledPositions, time: scheduledTime} = await measureExecutionTime(async () => await getScheduledVehicleLocations(agency, time));
+    const {result: livePositions, time: liveTime} = await measureExecutionTime(async () => await getLiveVehicleLocations(agency, time));
+
+    console.log("Scheduled execution time:", scheduledTime, "Live execution time:", liveTime)
 
     const output: LinkedPositionsOutput = {};
 
